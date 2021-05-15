509a81ff9963f378e58cd556ddfa1b921ce76880
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2021-05-15 15:27:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function changeColor()
-{
+changeColor = () => {
   const hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
   var hexcode1 = '';
   var hexcode2 = '';
