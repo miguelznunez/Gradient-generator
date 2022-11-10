c14b265ffd005c05319449b864a387da9660c69b
Miguel Nunez <71807189+iamcodefoxx@users.noreply.github.com>
2022-11-09 18:02:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-document.querySelector("#submit").addEventListener("click", () => {
-  var hex_numbers = ["0","1","2","3","4","5","6","7","8","9","A", "B", "C", "D", "E", "F"];
-  var hexcode1 = "";
-  var hexcode2 = "";
-  var random_index = 0;
-  
-  for(let i = 0; i < 6;i++){
-    random_index = Math.floor(Math.random() * hex_numbers.length);
-      hexcode1 += hex_numbers[random_index];
-    random_index = Math.floor(Math.random() * hex_numbers.length);
-      hexcode2 += hex_numbers[random_index];
-  }
+const hexNumbers = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+let hex1 = ""
+let hex2 = ""
 
-  document.body.style.background = `linear-gradient(to right, #${hexcode1}, #${hexcode2})`;
-  document.querySelector("#hexcode1").textContent = hexcode1;
-  document.querySelector("#hexcode2").textContent = hexcode2;
-});
+document.querySelector("#generate").addEventListener("click", () => {
+  generateRandomHex()
+})
+
+document.querySelector("#copy").addEventListener("click", () => {
+  copyHex()
+})
+
+function generateRandomHex(){
+  hex1 = "", hex2 = ""
+  for(let i = 0; i < 6; i++){
+    hex1 += hexNumbers[Math.floor(Math.random() * hexNumbers.length)]
+    hex2 += hexNumbers[Math.floor(Math.random() * hexNumbers.length)]
+  }
+  document.querySelector("#hex1").innerHTML = hex1
+  document.querySelector("#hex2").innerHTML = hex2
+  document.body.style.background = `linear-gradient(45deg, #${hex1}, #${hex2})`
+}
+
+function copyHex(){
+  const gradient = `background: linear-gradient(45deg, #${hex1}, #${hex2})`
+  navigator.clipboard.writeText(gradient)
+}
